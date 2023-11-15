03e9d2d092996b0542682dcd60c30b0bb01add5c
dwiiwahyu <145457742+dwiiwahyu@users.noreply.github.com>
2023-11-15 14:03:30 +0800

    Update hurdle 3.py

--- hurdle 3.py
+++ hurdle 3.py
@@ -1,1 +1,19 @@
-
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def pass_wall():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        pass_wall()
+    else:
+        move()
