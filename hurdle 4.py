b212442bfbeeeab03f1fff3d0ffd07815c74f1e5
roudhobr <144611660+roudhobr@users.noreply.github.com>
2023-11-16 21:38:32 +0700

    Update hurdle 4.py

--- hurdle 4.py
+++ hurdle 4.py
@@ -1,1 +1,22 @@
-
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def wall_front():
+    while wall_on_right():
+        move()
+    if right_is_clear():
+        turn_right()
+        move()
+        turn_right()
+        while front_is_clear():
+            move()
+     
+while not at_goal():
+    if wall_in_front():
+        turn_left()
+        wall_front()
+        turn_left()
+    else:
+        move()
